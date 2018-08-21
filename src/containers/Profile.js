a9c8df54b59a75ae96c96e93fefee84e91840225
Nourian <amin.nourian@gmail.com>
2018-08-21 21:11:53 +0430

    aaa

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,18 +1,33 @@
+/* global $ */
 import React from 'react'
+import ReactDOM from 'react-dom'
 import {Component} from 'react'
 import {Button} from '@material-ui/core'
 
-
 class Profile extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: "ssss",
+        };
+    }
     render() {
         return (
             <div>
                 <p>I am the Profile! wooow</p>
-                <Button onFocus={()=>{alert(1)}} onClick={()=>{alert('i am clicjed!')}} variant="contained">Click Me Bro!</Button>
+                <Button onClick={() => this.setState({value: 'X'})} variant="contained"> {this.state.value}</Button>
+                <input id={"ex1"} type="text"/>
             </div>
 
         );
     }
-}
+    componentDidMount(){
+        const node = ReactDOM.findDOMNode(this);
+        console.log( $(node))
+        $(document).ready(function() {
+            $(node).children("#ex1").pDatepicker();
+        });
 
+    }
+}
 export default Profile;
